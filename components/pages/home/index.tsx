96ba5bb3ef20028af4a5a55c2a8df65b41a339cd
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2025-03-01 00:33:31 +0100

    feat(pages): create home page

---
+++ components/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import Hero from "@/components/organisms/home/HeroSection";
+import NeuroArchitectureSection from "@/components/organisms/home/NeuroArchitectureSection";
+import AboutSection from "@/components/organisms/home/AboutSection";
+import ServicesSection from "@/components/organisms/home/ServicesSection";
+import ProcessSection from "@/components/organisms/home/ProcessSection";
+import PortfolioSection from "@/components/organisms/home/PortfolioSection";
+import QualificationSection from "@/components/organisms/home/QualificationSection";
+import FaqSection from "@/components/organisms/home/FaqSection";
+import CtaSection from "@/components/organisms/home/CtaSection";
+import Footer from "@/components/organisms/home/Footer";
+import Navbar from "@/components/organisms/home/Navbar";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <NeuroArchitectureSection />
+      <AboutSection />
+      <ServicesSection />
+      <ProcessSection />
+      <PortfolioSection />
+      <QualificationSection />
+      <FaqSection />
+      <CtaSection />
+      <Footer />
+    </main>
+  );
+}
