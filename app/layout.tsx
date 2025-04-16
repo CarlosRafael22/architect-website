2b9e0c97a25d91e65f95d02cab41f537fed01f23
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2025-04-16 21:41:23 +0200

    feat(app/layout): add Clarity config correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Montserrat, Cormorant_Garamond } from "next/font/google";
-import Clarity from "@microsoft/clarity";
+import Script from "next/script";
 import "./globals.css";
-
-Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID!);
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -26,6 +24,10 @@ export const metadata: Metadata = {
     "Transformando espaços através da neuroarquitetura - criando ambientes que elevam o bem-estar, a produtividade e as emoções através de soluções arquitetônicas baseadas em evidências.",
 };
 
+const shouldLoadClarity =
+  process.env.VERCEL_ENV === "preview" ||
+  process.env.VERCEL_ENV === "production";
+
 export default function RootLayout({
   children,
 }: {
@@ -33,6 +35,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      {shouldLoadClarity && (
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
+      )}
+
       <body
         className={`${montserrat.variable} ${cormorant.variable} font-sans`}
       >
